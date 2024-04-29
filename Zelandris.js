699af8a258fa8930e3ecf7c216236dfa2c91dd4c
R-kaloustian <102569106+Roupstrer02@users.noreply.github.com>
2024-04-29 13:21:08 -0400

    Update Zelandris.js

--- Zelandris.js
+++ Zelandris.js
@@ -4,13 +4,13 @@ function hoverStatDisplayer(buttonInList) {
     switch(buttonInList) {
         case 1:
             formulaDisplayer.innerText = "Max health = 100 + 5 * (Vitality - 1) + Total_Equipment_Max_Health * (1 + Vitality/100)";
-          break;
+            break;
         case 2:
             formulaDisplayer.innerText = "Healing Received% = 100 + Resilience \n HealthRegen/s = (1 + (Resilience - 1)/5 + Total_Equipment_HealthRegen) * Healing_Received%/100";
-          break;
+            break;
         case 3:
             formulaDisplayer.innerText = "Damage = Strength + Total_Equipment_Damage * (Strength/100)";
-        break;
+            break;
         case 4:
             formulaDisplayer.innerText = "MovementSpeed% = 100 + Dexterity/100 + Total_Equipment_MoveSpeed% \n MultiHit% = Dexterity/100 + Total_Equipment_MultiHit%";
             break;
