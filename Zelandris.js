8ca73e5558ad49d91baf6a76de728a4611de6617
R-kaloustian <102569106+Roupstrer02@users.noreply.github.com>
2025-06-26 20:09:31 -0400

    Content Update
    Makor Content update for Artisan page

--- Zelandris.js
+++ Zelandris.js
@@ -28,3 +28,34 @@ function hoverStatDisplayer(buttonInList) {
       }
 
 }
+
+function updateRecipeList() {
+      // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("ArtisanRecipeList");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("button")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+
+function toggleDisplay(target) {
+    description = target.children[0]
+    if (description.style.display == "none" || description.style.display == "") {
+        description.style.display = "flex";
+    } else {
+        description.style.display = "none";
+    }
+}
+
